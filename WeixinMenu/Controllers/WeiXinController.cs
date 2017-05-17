b5e7ef8971606b82aae01b9f71480b8aa33596d8
jackche <chechao3272@163.com>
2017-05-17 18:04:40 +0800

    开始菜单栏v0.0.2

---
+++ WeixinMenu/Controllers/WeiXinController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeixinMenu.Controllers
+{
+    public class WeiXinController : Controller
+    {
+        // GET: WeiXin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        #region"微信菜单管理"
+        public ActionResult Menu()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
